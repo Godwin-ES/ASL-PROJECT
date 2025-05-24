73ad600c2b270475684cb15707995b5cac7f4ea9
Godwin Ekanem <ekanemgodwins@gmail.com>
2025-05-24 15:00:12 +0100

    Edit

--- backend/stt.py
+++ backend/stt.py
@@ -1,10 +1,23 @@
 # backend/stt.py
 
 import io
-from pydub import AudioSegment
+#from pydub import AudioSegment
 import speech_recognition as sr
 
 def speech_to_text(audio_bytes: bytes) -> str:
+    try:
+        recognizer = sr.Recognizer()
+        audio_file = io.BytesIO(audio_bytes)
+
+        with sr.AudioFile(audio_file) as source:
+            audio_data = recognizer.record(source)
+            text = recognizer.recognize_google(audio_data)
+
+        return text.upper()
+    except Exception as e:
+        print(f"[STT ERROR] {e}")
+        return f"[STT ERROR] {e}"    
+    '''
     try:
         # Convert to WAV using pydub
         audio_segment = AudioSegment.from_file(io.BytesIO(audio_bytes))
@@ -22,3 +35,4 @@ def speech_to_text(audio_bytes: bytes) -> str:
     except Exception as e:
         print(f"[STT ERROR] {e}")
         return f"[STT ERROR] {e}"
+    '''
